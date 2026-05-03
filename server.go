16a52405f02f6766ca23c14ede74545a703fcefb
gooneraki <hayiate4@hotmail.com>
2026-05-03 10:32:11 +0300

    replace fmt with log for consistent logging in auth, handlers, main, and server files

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -48,7 +49,7 @@ func (s *server) start() error {
 		return err
 	}
 
-	fmt.Printf("Linko is running on http://localhost:%d\n", ln.Addr().(*net.TCPAddr).Port)
+	log.Printf("Linko is running on http://localhost:%d", ln.Addr().(*net.TCPAddr).Port)
 
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
@@ -57,7 +58,7 @@ func (s *server) start() error {
 }
 
 func (s *server) shutdown(ctx context.Context) error {
-	fmt.Println("Linko is shutting down")
+	log.Println("Linko is shutting down")
 	return s.httpServer.Shutdown(ctx)
 }
 
