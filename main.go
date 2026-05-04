b18f94159746d8b83d47154e9d01b45b35acd8f6
gooneraki <hayiate4@hotmail.com>
2026-05-04 19:26:03 +0300

    refactor: enhance logger initialization by adding buffered writing for improved performance

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
@@ -63,10 +64,13 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 func initializeLogger(logFile string) (*log.Logger, error) {
 	if logFile != "" {
 		file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
+
 		if err != nil {
 			return nil, fmt.Errorf("failed to open log file: %w", err)
 		}
-		multiWriter := io.MultiWriter(os.Stderr, file)
+		bufferedFile := bufio.NewWriterSize(file, 8192)
+
+		multiWriter := io.MultiWriter(os.Stderr, bufferedFile)
 		return log.New(multiWriter, "", log.LstdFlags), nil
 	}
 	return log.New(os.Stderr, "", log.LstdFlags), nil
