3268f6d10a20f3913cf8d3c6b5f007f975bc5372
gooneraki <hayiate4@hotmail.com>
2026-05-08 14:24:11 +0300

    refactor: enhance logging to include username in request context

--- auth.go
+++ auth.go
@@ -12,6 +12,11 @@ import (
 type contextKey string
 
 const UserContextKey contextKey = "user"
+const logContextKey contextKey = "log_context"
+
+type LogContext struct {
+	Username string
+}
 
 var allowedUsers = map[string]string{
 	"frodo":   "$2a$10$B6O/n6teuCzpuh66jrUAdeaJ3WvXcxRkzpN0x7H.di9G9e/NGb9Me",
@@ -45,6 +50,9 @@ func (s *server) authMiddleware(next http.Handler) http.Handler {
 		if !ok {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
+		}
+		if logCtx, ok := r.Context().Value(logContextKey).(*LogContext); ok {
+			logCtx.Username = username
 		}
 		r = r.WithContext(context.WithValue(r.Context(), UserContextKey, username))
 		next.ServeHTTP(w, r)
