dceb6f2fb41940115f820fd81a83910c79e6bfc5
gooneraki <hayiate4@hotmail.com>
2026-08-01 17:53:30 +0300

    feat: update docker-compose and Prometheus configuration for enhanced metrics collection

--- server.go
+++ server.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"boot.dev/linko/internal/store"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type server struct {
@@ -168,6 +169,7 @@ func newServer(store store.Store, port int, logger *slog.Logger, cancel context.
 	mux.Handle("GET /api/urls", s.authMiddleware(http.HandlerFunc(s.handlerListURLs)))
 	mux.HandleFunc("GET /{shortCode}", s.handlerRedirect)
 	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
+	mux.Handle("GET /metrics", promhttp.Handler())
 
 	return s
 }
